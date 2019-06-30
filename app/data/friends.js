0e97b9a1face3ed4dad056c1772b6ab8092d52de
Chynna Johnson <chynnajohnson@Chynnas-MacBook-Pro.local>
2019-06-30 09:35:42 -0500

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,24 @@
 // Export to API Route
 var friendsList = [
+  {
+    name: "carlton",
+    photo:
+      "https://pixel.nymag.com/imgs/daily/vulture/2018/10/16/17-Alfonso-Ribeiro.w330.h330.jpg",
+    preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+  },
 
+  {
+    name: "stewie",
+    photo:
+      "https://vignette.wikia.nocookie.net/familyguyfanon/images/0/02/Stewie_Griffin.png/revision/latest?cb=20161119043926",
+    preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+  },
+
+  {
+    name: "cleveland",
+    photo: "https://upload.wikimedia.org/wikipedia/en/e/e4/Cleveland_Brown.png",
+    preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  }
 ];
 
 module.exports = friendsList;
